51e2ed86f3e19b571fc94ce6abb6d47105110749
SergeEvdokimov <SErgeyEwdokimow@yandex.ru>
2020-05-11 14:25:02 +0300

    project finished

---
+++ Graph/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vertex1 = new System.Windows.Forms.Button();
+            this.vertex2 = new System.Windows.Forms.Button();
+            this.vertex3 = new System.Windows.Forms.Button();
+            this.vertex4 = new System.Windows.Forms.Button();
+            this.vertex5 = new System.Windows.Forms.Button();
+            this.vertex6 = new System.Windows.Forms.Button();
+            this.vertex7 = new System.Windows.Forms.Button();
+            this.vertex8 = new System.Windows.Forms.Button();
+            this.vertex9 = new System.Windows.Forms.Button();
+            this.vertex10 = new System.Windows.Forms.Button();
+            this.vertex11 = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnAgain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vertex1
+            // 
+            this.vertex1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex1.Location = new System.Drawing.Point(9, 6);
+            this.vertex1.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex1.Name = "vertex1";
+            this.vertex1.Size = new System.Drawing.Size(59, 63);
+            this.vertex1.TabIndex = 0;
+            this.vertex1.UseVisualStyleBackColor = false;
+            this.vertex1.Click += new System.EventHandler(this.vertex1_Click);
+            // 
+            // vertex2
+            // 
+            this.vertex2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex2.Location = new System.Drawing.Point(110, 6);
+            this.vertex2.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex2.Name = "vertex2";
+            this.vertex2.Size = new System.Drawing.Size(59, 63);
+            this.vertex2.TabIndex = 1;
+            this.vertex2.UseVisualStyleBackColor = false;
+            this.vertex2.Click += new System.EventHandler(this.vertex2_Click);
+            // 
+            // vertex3
+            // 
+            this.vertex3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex3.Location = new System.Drawing.Point(9, 162);
+            this.vertex3.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex3.Name = "vertex3";
+            this.vertex3.Size = new System.Drawing.Size(59, 63);
+            this.vertex3.TabIndex = 2;
+            this.vertex3.UseVisualStyleBackColor = false;
+            this.vertex3.Click += new System.EventHandler(this.vertex3_Click);
+            // 
+            // vertex4
+            // 
+            this.vertex4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex4.Location = new System.Drawing.Point(9, 309);
+            this.vertex4.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex4.Name = "vertex4";
+            this.vertex4.Size = new System.Drawing.Size(59, 63);
+            this.vertex4.TabIndex = 3;
+            this.vertex4.UseVisualStyleBackColor = false;
+            this.vertex4.Click += new System.EventHandler(this.vertex4_Click);
+            // 
+            // vertex5
+            // 
+            this.vertex5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex5.Location = new System.Drawing.Point(359, 309);
+            this.vertex5.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex5.Name = "vertex5";
+            this.vertex5.Size = new System.Drawing.Size(59, 63);
+            this.vertex5.TabIndex = 4;
+            this.vertex5.UseVisualStyleBackColor = false;
+            this.vertex5.Click += new System.EventHandler(this.vertex5_Click);
+            // 
+            // vertex6
+            // 
+            this.vertex6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex6.Location = new System.Drawing.Point(359, 10);
+            this.vertex6.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex6.Name = "vertex6";
+            this.vertex6.Size = new System.Drawing.Size(59, 63);
+            this.vertex6.TabIndex = 5;
+            this.vertex6.UseVisualStyleBackColor = false;
+            this.vertex6.Click += new System.EventHandler(this.vertex6_Click);
+            // 
+            // vertex7
+            // 
+            this.vertex7.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex7.Location = new System.Drawing.Point(512, 10);
+            this.vertex7.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex7.Name = "vertex7";
+            this.vertex7.Size = new System.Drawing.Size(59, 63);
+            this.vertex7.TabIndex = 6;
+            this.vertex7.UseVisualStyleBackColor = false;
+            this.vertex7.Click += new System.EventHandler(this.vertex7_Click);
+            // 
+            // vertex8
+            // 
+            this.vertex8.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex8.Location = new System.Drawing.Point(512, 162);
+            this.vertex8.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex8.Name = "vertex8";
+            this.vertex8.Size = new System.Drawing.Size(59, 63);
+            this.vertex8.TabIndex = 7;
+            this.vertex8.UseVisualStyleBackColor = false;
+            this.vertex8.Click += new System.EventHandler(this.vertex8_Click);
+            // 
+            // vertex9
+            // 
+            this.vertex9.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex9.Location = new System.Drawing.Point(512, 309);
+            this.vertex9.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex9.Name = "vertex9";
+            this.vertex9.Size = new System.Drawing.Size(59, 63);
+            this.vertex9.TabIndex = 8;
+            this.vertex9.UseVisualStyleBackColor = false;
+            this.vertex9.Click += new System.EventHandler(this.vertex9_Click);
+            // 
+            // vertex10
+            // 
+            this.vertex10.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex10.Location = new System.Drawing.Point(676, 309);
+            this.vertex10.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex10.Name = "vertex10";
+            this.vertex10.Size = new System.Drawing.Size(59, 63);
+            this.vertex10.TabIndex = 9;
+            this.vertex10.UseVisualStyleBackColor = false;
+            this.vertex10.Click += new System.EventHandler(this.vertex10_Click);
+            // 
+            // vertex11
+            // 
+            this.vertex11.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.vertex11.Location = new System.Drawing.Point(676, 413);
+            this.vertex11.Margin = new System.Windows.Forms.Padding(2);
+            this.vertex11.Name = "vertex11";
+            this.vertex11.Size = new System.Drawing.Size(59, 63);
+            this.vertex11.TabIndex = 10;
+            this.vertex11.UseVisualStyleBackColor = false;
+            this.vertex11.Click += new System.EventHandler(this.vertex11_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.White;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSearch.Location = new System.Drawing.Point(746, 178);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(131, 24);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Найти путь!";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(746, 25);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(132, 139);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "Выделите две вершины, между которыми хотите найти кротчайший путь, а потом нажмит" +
+    "е \"Найти путь!\"";
+            // 
+            // btnAgain
+            // 
+            this.btnAgain.BackColor = System.Drawing.Color.White;
+            this.btnAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAgain.Location = new System.Drawing.Point(729, 214);
+            this.btnAgain.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgain.Name = "btnAgain";
+            this.btnAgain.Size = new System.Drawing.Size(163, 34);
+            this.btnAgain.TabIndex = 13;
+            this.btnAgain.Text = "Начать заново!";
+            this.btnAgain.UseVisualStyleBackColor = false;
+            this.btnAgain.Click += new System.EventHandler(this.btnAgain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(946, 558);
+            this.Controls.Add(this.btnAgain);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.vertex11);
+            this.Controls.Add(this.vertex10);
+            this.Controls.Add(this.vertex9);
+            this.Controls.Add(this.vertex8);
+            this.Controls.Add(this.vertex7);
+            this.Controls.Add(this.vertex6);
+            this.Controls.Add(this.vertex5);
+            this.Controls.Add(this.vertex4);
+            this.Controls.Add(this.vertex3);
+            this.Controls.Add(this.vertex2);
+            this.Controls.Add(this.vertex1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button vertex1;
+        private System.Windows.Forms.Button vertex2;
+        private System.Windows.Forms.Button vertex3;
+        private System.Windows.Forms.Button vertex4;
+        private System.Windows.Forms.Button vertex5;
+        private System.Windows.Forms.Button vertex6;
+        private System.Windows.Forms.Button vertex7;
+        private System.Windows.Forms.Button vertex8;
+        private System.Windows.Forms.Button vertex9;
+        private System.Windows.Forms.Button vertex10;
+        private System.Windows.Forms.Button vertex11;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnAgain;
+    }
+}
+
---
+++ Graph/Vertex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph
+{
+    public class Vertex
+    {
+        private States state;
+        private Button btn;
+
+        public States State
+        {
+            get
+            {
+                return state;
+            }
+        }
+        public Button Btn
+        {
+            get
+            {
+                return btn;
+            }
+        }
+        public Vertex(Button _btn)
+        {
+            btn = _btn;
+            state = States.medium;
+            
+        }
+        public Vertex(Button _btn, States _state)
+        {
+            btn = _btn;
+            state = _state;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var vertex = obj as Vertex;
+            return vertex != null &&
+                    EqualityComparer<Button>.Default.Equals(btn, vertex.btn) &&
+                    state == vertex.state &&
+                    EqualityComparer<Button>.Default.Equals(Btn, vertex.Btn) &&
+                            State == vertex.State;
+        }
+    }
+}
